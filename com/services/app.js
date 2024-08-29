39cf02c1e3a6b7bcfd23e0d342af22dd610b7720
Jared Hanson <jaredhanson@gmail.com>
2024-08-29 16:04:17 -0700

    Load YAML file.

--- com/services/app.js
+++ com/services/app.js
@@ -1,6 +1,16 @@
+var path = require('path');
+var fs = require('fs');
 var YAMLServiceRegistry = require('../../lib/yamlserviceregistry');
 
 exports = module.exports = function() {
   var registry = new YAMLServiceRegistry();
+  
+  var fname = path.resolve(__dirname, '../../etc/services.yaml')
+    , data;
+  if (fs.existsSync(fname)) {
+    data = fs.readFileSync(fname, 'utf8');
+    registry.load(data);
+  }
+  
   return registry;
 };
--- test/services/app.test.js
+++ test/services/app.test.js
@@ -8,4 +8,11 @@ var factory = require('../../com/services/app');
 
 describe('services/app', function() {
   
+  it('should load service configuration from this package', function() {
+    var registry = factory();
+    console.log(registry);
+    
+    expect(1).to.equal(2);
+  }); // should load service configuration from this package
+  
 });
