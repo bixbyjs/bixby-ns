65ee610e27e488e7641632adc59c2bdb03097fec
Jared Hanson <jaredhanson@gmail.com>
2024-09-14 10:24:07 -0700

    Add comments.

--- com/services.js
+++ com/services.js
@@ -1,5 +1,8 @@
 var PluggableServiceRegistry = require('../lib/pluggableserviceregistry');
 
+
+// https://www.iana.org/assignments/service-names-port-numbers
+// /etc/services
 exports = module.exports = function(app) {
   var registry = new PluggableServiceRegistry();
   registry.use(app);
