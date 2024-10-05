c39e0e5950c59a05ea5efa03651aa0e300d5ab80
Jared Hanson <jaredhanson@gmail.com>
2024-10-05 11:04:45 -0700

    Add comments.

--- com/services/app.js
+++ com/services/app.js
@@ -1,7 +1,20 @@
+// Module dependencies.
 var path = require('path');
 var fs = require('fs');
 var YAMLServiceRegistry = require('../../lib/yamlserviceregistry');
 
+/**
+ * Create application-specific service registry.
+ *
+ * Returns a service registry that is configured with information about network
+ * services that is loaded from application-specific configuration files, as
+ * well as commonly used services loaded from configuraiton files supplied with
+ * this package.
+ *
+ * 
+ *
+ * @returns {bixby-ns.ServiceRegistry}
+ */
 exports = module.exports = function() {
   var registry = new YAMLServiceRegistry();
   
